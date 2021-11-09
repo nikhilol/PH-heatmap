550120304d7939a526bcc5dab4d9150a0b53bb55
Nikhil <nikhil.patel.np@outlook.com>
2021-11-09 14:54:59 +0000

    Styling changes

--- ph-heatmap/src/App.js
+++ ph-heatmap/src/App.js
@@ -12,20 +12,20 @@ function App() {
   function dateConfig(){
 		var now = new Date();
     let temp=[]
-		for (var d = new Date(2021, 0, 1); d <= now; d.setDate(d.getDate() + 1)) {
+		for (var d = new Date(2020, 7, 11); d <= now; d.setDate(d.getDate() + 1)) {
       temp.push(new Date(d))
 		}
-    setDaysOfYear(temp);
+    setDaysOfYear(temp.slice(0).slice(-365));
   }
 
 	return (
     <>
-		<div className="App" style={{display:'flex', justifyContent:'center', width:'100vw'}}>
-			{daysOfYear.length > 300 && (
-				<div style={{width:'80%', marginBottom:'50vh'}}>
+		<div className="App" style={{display:'flex', justifyContent:'center', width:'100vw', alignItems:'center', height:'100vh'}}>
+			{daysOfYear.length == 365 && (
+				<div style={{width:'80%', marginBottom:'50vh', position:'relative'}}>
 					{daysOfYear.map((date, i) => {
             let index = i + daysOfYear[0].getDay()
-						return <div style={{width:'15px', height:'15px', background:'#eeffee', borderRadius:'25%', margin:'4px', position:'absolute', top:(index%7)*19, left:Math.floor(index/7)*19}}></div>;
+						return <div className='DateSquare' style={{top:(index%7)*19, left:Math.floor(index/7)*19, background: `rgba(246, 73, 0, ${Math.random()*0.7 +0.1})` }}></div>;
 					})}
 				</div>
 			)}
