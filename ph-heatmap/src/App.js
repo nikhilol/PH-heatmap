4a1862874839eee4b389b86997e974ef3fa94b5e
Nikhil <nikhil.patel.np@outlook.com>
2021-11-11 13:33:14 +0000

    small styling changes

--- ph-heatmap/src/App.js
+++ ph-heatmap/src/App.js
@@ -41,7 +41,7 @@ function App() {
 		</div>
 		<h1 style={{borderBottom:'1px solid #DA552F', color:'#DA552F', paddingBottom:'2vh'}}>My Product Hunt <span style={{color:'#1A202C', background:'#DA552F', borderRadius:'2px', padding:'2px 5px'}}>365</span></h1>
 		<p style={{width:'40%', fontWeight:'600'}}>👋Hey! I'm Nikhil, welcome!</p>
-		<p style={{width:'30%', fontWeight:'600'}}>💡 The idea for this project is to build a Github style heatmap of my favourite products every day for a year! I will also be posting every choice I make on <a href='https://twitter.com/Niikhiil_P'>Twitter</a>, so why not follow me there! 👽</p>
+		<p style={{width:'40%', fontWeight:'600'}}>💡 The idea for this project is to build a GitHub style heatmap of my favourite products every day for a year! I will also be posting every choice I make on <a href='https://twitter.com/Niikhiil_P'>Twitter</a>, so follow me over there for updates! 👽</p>
 		<div style={{display:'flex', width:'100vw', justifyContent:'center', alignItems:'flex-start', height: 7*multiplier+'vw', marginTop:'10vh', zIndex:'5'}}>
 			{daysOfYear.length === 365 && (
 				<ClickAwayListener onClickAway={()=>{setActive(null)}}>
